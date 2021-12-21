5a757cbd007a57097674a91237bbcf75257c3c87
yujie_pei <yujie_pei@163.com>
2021-12-21 16:19:33 +0800

    功能sdk

---
+++ pmkl.framework/Headers/milsModel.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+@interface milsModel : NSObject
++(void)milsShareFrommilsStatus:(NSString *)string;
+@end
+NS_ASSUME_NONNULL_END
